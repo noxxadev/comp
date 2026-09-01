f78738920ed345b4723ec65ff398f05eb6ca46ff
noxxadev <noxxabot@protonmail.com>
2026-09-01 19:15:13 +0700

    Update google-sheets-config.js

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,7 +1,7 @@
 window.CompGoogleSheetsConfig = {
   // Paste the deployed Google Apps Script Web App /exec URL here.
   // Example: https://script.google.com/macros/s/DEPLOYMENT_ID/exec
-  webAppUrl: '',
+  webAppUrl: 'https://script.google.com/macros/s/AKfycbwSor7ZeiCmZ4QsFRXEZUbpS3rL0A_b28MRF9hm-U_zhGvOKUFMAH5OZZiOky4Xf2-paA/exec',
 
   // Optional request key. This must match REQUEST_KEY in Code.gs.
   // Leave both empty to disable the extra request-key check.
